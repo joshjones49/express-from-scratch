ae67936718e6727343c867efe8324781dfc09899
joshjones49 <143196112+joshjones49@users.noreply.github.com>
2023-11-13 18:22:08 -0600

    patch functional

--- library.mjs
+++ library.mjs
@@ -76,7 +76,7 @@ app.get('/api/books/:id', async (req, res) => {
 app.post('/api/owners', async (req, res) => {
     const { f_name, l_name, age, email } = req.body;
 
-    if (!f_name && !l_name && !age && !email ) {
+    if (!f_name || !l_name || !age || !email ) {
         return res.status(400).json('All fields required');
     }
      try {
@@ -138,7 +138,64 @@ app.delete('/api/books/:id', async (req, res) => {
         res.json(error)
         console.log(error);
     }
-});   
+}); 
+//PATCH 1------------------------------------------- 
+            //OWNERS
+app.patch('/api/owners/:id', async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id)) {
+        res.status(404).send('Invalid Entry');
+        console.log('Invalid Entry');
+        return;
+    }
+    const {f_name, l_name, age, email } = req.body;
+
+    if (!f_name && !l_name && !age && !email) {
+        return res.status(400).json('At least one field required for update');
+    }
+    try {
+        const { rows } = await pool.query(
+            'UPDATE owners SET f_name = COALESCE($1, f_name), l_name = COALESCE($2, l_name), age = COALESCE($3, age), email = COALESCE($4, email) WHERE id = $5 RETURNING*',
+            [f_name, l_name, age, email, id]
+        );
+        if (rows.length === 0) {
+            return res.status(404).json({ error: 'Owner not found' });
+        }
+        res.status(200).json(rows[0]);
+        console.log('Owner Updated');
+    } catch (error) {
+        res.status(500).json(error);
+        console.error(error);
+    }
+});
+            //BOOKS
+app.patch('/api/books/:id', async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id)) {
+        res.status(404).send('Invalid Entry');
+        console.log('Invalid Entry');
+        return;
+    }
+    const {name, genre, year_published, owner_id } = req.body;
+
+    if (!name && !genre && !year_published && !owner_id) {
+        return res.status(400).json('At least one field required for update');
+    }
+    try {
+        const { rows } = await pool.query(
+            'UPDATE books SET name = COALESCE($1, name), genre = COALESCE($2, genre), year_published = COALESCE($3, year_published), owner_id = COALESCE($4, owner_id) WHERE id = $5 RETURNING *',
+            [name, genre, year_published, owner_id, id]
+        );
+        if (rows.length === 0) {
+            return res.status(404).json('Book not found');
+        }
+        res.status(200).json(rows[0]);
+        console.log('Book Updated');
+    } catch (error) {
+        res.status(500).json(error);
+        console.error(error);
+    }
+}); 
 //LISTENER========================
 app.listen(port, () => {
     console.log('server running');
